a4d4294f83fc970cf870f71f84f0ffddb176339d
Nazmul Huda <shawonnazmulhuda@gmail.com>
2023-06-10 18:25:49 +0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+
+function Footer({ socialLinks }) {
+  return (
+    <FooterWrapper>
+      <div className="container py-3 text-center">                       
+            {socialLinks.map(link => (
+              <a className="mx-2" key={link.id} href={link.url}>
+                {link.icon}
+              </a>
+              
+            ))}
+              <p className="mt-3">
+              copyright &copy; Gadget BD 2023 all rights
+              reserved
+            </p>
+          </div>
+    </FooterWrapper>
+  );
+}
+const mapStatesToProps = ({ products }) => {
+  return { socialLinks: products.socialLinks };
+};
+
+const FooterWrapper = styled.footer`
+  background: var(--darkGrey);
+  color: var(--mainWhite);
+  .icon {
+    font-size: 1.5rem;
+    color: var(--mainWhite);
+    transition: var(--mainTransition);
+  }
+  .icon:hover {
+    color: var(--primaryColor);
+    cursor: pointer;
+  }
+`;
+
+export default connect(mapStatesToProps, null)(Footer);
---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import {Link}  from "react-router-dom";
+import { FaBars, FaCartPlus } from "react-icons/fa";
+import { handleSidebar, handleCart } from "../store/actions/products";
+
+function Navbar(props) {
+  return (
+    <NavWrapper>
+      <div className="nav-center">
+        <h3>Gadget BD</h3>
+        <div className="nav-cart">
+          <Link to="/">login</Link>
+          <FaCartPlus className="nav-icon" onClick={props.handleCart} />
+          <div className="cart-items">{props.cartItems}</div>
+        </div>
+      </div>
+    </NavWrapper>
+  );
+}
+const NavWrapper = styled.nav`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  padding: 1rem 1.5rem;
+  background: var(--mainGrey);
+  border-bottom: 3px solid var(--primaryColor);
+  z-index: 1;
+  .nav-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+  .nav-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+  .nav-cart {
+    position: relative;
+  }
+  .cart-items {
+    position: absolute;
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    font-size: 0.85rem;
+    top: -8px;
+    right: -8px;
+    padding: 0 5px;
+    border-radius: 50%;
+  }
+`;
+
+const mapStatesToProps = state => {
+  return { cartItems: state.products.cartItems };
+};
+
+export default connect(mapStatesToProps, { handleSidebar, handleCart })(Navbar);
